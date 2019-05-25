2946a535fd68ac489c0afac2f2cf4ae5436ee0f7
xiaoming <15xmchen@stu.edu.cn>
2019-05-25 20:52:17 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,4 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var price = 6 + 0.25 * inputs.parkTime + Math.min(6, Math.max(0, inputs.distance - 2)) * 0.8 + Math.max(0, inputs.distance - 8) * 1.2;
+    return Math.round(price);
 };
